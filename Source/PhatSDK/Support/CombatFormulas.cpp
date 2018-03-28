9ddc24a03faf78890aa8eda7542b9dd6441ed236
Dekaru <dekaru@protonmail.com>
2018-03-28 14:16:33 -0300

    - Changed bonus to damage from attribute to flatten off at higher levels instead of being linear.

--- Source/PhatSDK/Support/CombatFormulas.cpp
+++ Source/PhatSDK/Support/CombatFormulas.cpp
@@ -62,7 +62,8 @@ void CalculateAttributeDamageBonus(DamageEventData *dmgEvent)
 		else
 			dmgEvent->source->m_Qualities.InqAttribute(STRENGTH_ATTRIBUTE, attrib, FALSE);
 
-		double attribDamageMod = ((int)attrib - 55.0) / 33.0;
+		//double attribDamageMod = ((int)attrib - 55.0) / 33.0;
+		double attribDamageMod = 6.75*(1.0 - exp(-0.005*((int)attrib - 55)));
 		if (attribDamageMod < 0)
 			dmgEvent->attributeDamageBonus = dmgEvent->baseDamage * (attribDamageMod / 2.0);
 		else
