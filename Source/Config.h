eae326abd71d16cca947c4de63fc42d9cfbeefd8
Walt Matuszak <walt@waltmatuszak.com>
2018-04-03 23:29:56 -0400

    Minor change to custom config options
    Added all the spells I missed when priming new character spellbook is used. Added an T0 for PK trophies and a minimum level for rewarding trophies.

--- Source/Config.h
+++ Source/Config.h
@@ -77,6 +77,7 @@ public:
 	virtual bool DisableUnassignedXPAtMaxLevel() { return m_bDisableUnassignedXPAtMaxLevel; };
 	virtual double VitaeXPMultiplier() { return m_fVitaeXPMultiplier; }
 	
+	virtual unsigned int PKTrophyStartingLevel() { return m_PKTrophyStartingLevel; };
 	virtual unsigned int PKTrophyID(int level);
 
 	virtual bool EnableSpellFociStarterPack() { return m_bEnableSpellFociStarterPack; };
@@ -165,6 +166,8 @@ protected:
 	bool m_bDisableUnassignedXPAtMaxLevel = false;
 	double m_fVitaeXPMultiplier = 1.0;
 
+	unsigned int m_PKTrophyStartingLevel = 1;
+	unsigned int m_PKTrophyIDT0 = 0;
 	unsigned int m_PKTrophyIDT1 = 0;
 	unsigned int m_PKTrophyIDT2 = 0;
 	unsigned int m_PKTrophyIDT3 = 0;
